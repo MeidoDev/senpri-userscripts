0c2fa6331f767723b516f18f41b6a9e2abf6376c
MeidoDev <MeidoDev@users.noreply.github.com>
2021-08-09 14:06:53 +0200

    Fix formatting in senpri.ui.js

--- senpri.ui.js
+++ senpri.ui.js
@@ -11,41 +11,42 @@
 /**
  * Functionality:
  *
- *	- Allow selecting text in guildchat and announcement sections
+ *  - Allow selecting text in guildchat and announcement sections
  *
  */
 
 window.SenpriUI = (function() {
-	"use strict";
+    "use strict";
 
-	var
+    var
 
-	setSelectable = function() {
-		$('.groupBoardArea__postText--bodyinner').css('user-select', 'text');
-		$('.mypageAdminInfoArea__detailTextArea').css('user-select', 'text');
-		$('.mypageAdminInfoArea__detailTextArea').children().css('user-select', 'text');
-	},
+    setSelectable = function() {
+        $('.groupBoardArea__postText--bodyinner').css('user-select', 'text');
+        $('.mypageAdminInfoArea__detailTextArea').css('user-select', 'text');
+        $('.mypageAdminInfoArea__detailTextArea').children().css('user-select', 'text');
+    },
     moveRaidListRefreshButton = function() {
         $('a.eventRaidBossList__refreshBtn').insertAfter('div.eventRaidBossList__finder--self');
     };
 
-	return {
-		init: function() {
-			setSelectable();
+    return {
+        init: function() {
+            setSelectable();
 
-			// Install an event listener for content that's injected via AJAX
-			$(document).ajaxSuccess(function() {
-				setSelectable();
-			});
+            // Install an event listener for content that's injected via AJAX
+            $(document).ajaxSuccess(function() {
+                setSelectable();
+            });
 
             // URL-specific stuff
             const curURL = new URL(window.location);
+            
             if (curURL.searchParams.get('c') === 'EventRaid' && curURL.searchParams.get('f') === 'bossList') {
                 moveRaidListRefreshButton();
             }
 
-		}
-	};
+        }
+    };
 })();
 
 window.SenpriUI.init();
